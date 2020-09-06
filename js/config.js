38b64df6ea2f08ce2b005bb3ec555a58a8d0eb83
Karabo Moichela <60990711+PeterMoich@users.noreply.github.com>
2020-09-06 16:30:42 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+
+
+
+window._config = {
+    cognito: {
+        userPoolId:  'us-east-1_LZIH8YL9w', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '5j8u784rsk5mcfo9mc9h5o4r1c' //is this used anywhere?
+    },
+};
